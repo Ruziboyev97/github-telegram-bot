f78caf915556b7ce6d2f644371cf3b3d7831bd51
Ruziboyev97 <97209027+Ruziboyev97@users.noreply.github.com>
2025-10-11 19:42:05 +0000

    testing

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ from dataclasses import dataclass
 @dataclass
 class Config:
     """Sozlamar"""
-    BOT_TOKEN: str = os.getenv("BOT_TOKEN", "TOKEN_YO'Q")
-    GITGUB_API_URL: str = "https://api.github.com"
+    BOT_TOKEN: str = os.getenv("BOT_TOKEN")
+    GITHUB_API_URL: str = "https://api.github.com"
     MAX_REPOS_DISPLAY: int = 10
 
     #malumotlar bazasi sekretlari
-    SUPABASE_URL: str = os.getenv("SUPABASE_URL", "MA'LUMOTLAR BAZASI URLSI")
-    SUPABASE_KEY: str = os.getenv("SUPABASE_KEY", "MA'lumotlar bazasi kaliti")
+    SUPABASE_URL: str = os.getenv("SUPABASE_URL")
+    SUPABASE_KEY: str = os.getenv("SUPABASE_KEY")
 
     #shifr kaliti
     ENCRYPRION_KEY = os.getenv("ENCRYPTION_KEY")
